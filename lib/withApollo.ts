cc4e8f3ee440fdac27af58b7b9da9056c5301044
daaasbu <danaasbury@gmail.com>
2020-01-13 22:49:22 -0600

    feat: add basic switch UI, migrate switches to have variants

--- lib/withApollo.ts
+++ lib/withApollo.ts
@@ -1,7 +1,7 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient, { InMemoryCache } from 'apollo-boost';
 
-const GRAPHQL_URL = 'localhost:4000/graphql';
+const GRAPHQL_URL = 'http://localhost:4000';
 
 export default withApollo(
     ({ initialState }) =>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,18 @@ import * as React from 'react'
 import Link from 'next/link'
 import Layout from '../components/Layout'
 import { NextPage } from 'next'
+import { useQuery } from '@apollo/react-hooks'
+import { GET_SWITCHES_BY_ORG, SwitchFromOrg } from '../gql/switches'
+import LightSwitch from '../components/Switch'
 
 const IndexPage: NextPage = () => {
+    const id = "ck5dcn0980000ixw1e9fjiqfh";
+    const { data, loading, error } = useQuery<{ switches: SwitchFromOrg[] }>(GET_SWITCHES_BY_ORG, { variables: { id } });
+
+    if (loading) return <p>Loading</p>;
+    if (error) return <p>ERROR: {error.message}</p>;
+    if (!data) return <p>Not found</p>;
+    const { switches } = data;
     return (
         <Layout title="Home | Next.js + TypeScript Example">
             <h1>Hello Next.js 👋</h1>
@@ -12,7 +22,12 @@ const IndexPage: NextPage = () => {
                     <a>About</a>
                 </Link>
             </p>
-        </Layout>
+            <React.Fragment>
+                {
+                    switches.map(lightswitch => <LightSwitch lightswitch={lightswitch}></LightSwitch>)
+                }
+            </React.Fragment>
+        </Layout >
     )
 }
 
